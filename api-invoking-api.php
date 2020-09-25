57eeb1027e37a719968c6ea8cf455e2a7f02fd66
McCoy Zhu <mz1945@nyu.edu>
2020-09-24 22:34:55 -0700

    Testing with api-invoking-api

---
+++ api-invoking-api.php
@@ -0,0 +1,18 @@
+<?php
+$params = array();
+parse_str($_SERVER['QUERY_STRING'], $params);
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, "https://yyq5828qhf.execute-api.us-east-1.amazonaws.com/dev/1/?" . $_SERVER['QUERY_STRING']);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+$output = curl_exec($curl);
+curl_close($curl);
+
+$response = new \stdClass;
+$response->time = date(DATE_ISO8601);
+$response->query = $params;
+$response->api_response = json_decode($output);
+
+$json_response = json_encode($response);
+echo $json_response;
+?>
